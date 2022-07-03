eddd82e1303a686a03694068aa9bd82fdd467be6
Anocha <62020796@up.ac.th>
2022-07-04 02:52:05 +0700

    commit

--- app/lib/questions4.dart
+++ app/lib/questions4.dart
@@ -1,4 +1,10 @@
-import 'package:flutter/material.dart'; // TODO Implemen
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:scroll_snap_list/scroll_snap_list.dart'; // TODO Implemen
 
 class Question4Widget extends StatefulWidget {
   const Question4Widget({Key? key}) : super(key: key);
@@ -10,6 +16,64 @@ class Question4Widget extends StatefulWidget {
 class _Question4WidgetState extends State<Question4Widget> {
   get child => null;
   int currentIndex = 0;
+  final screens = [
+    Center(
+      child: Text("test"),
+    ),
+    Center(
+      child: Text("test2"),
+    ),
+    Center(
+      child: Text("test3"),
+    ),
+    // EX Question4Widget();
+  ];
+  List<int> data = [];
+  int _focusedIndex = 0;
+  void initState() {
+    super.initState();
+
+    for (int i = 0; i < 30; i++) {
+      data.add(Random().nextInt(100) + 1);
+    }
+  }
+
+  void _onItemFocus(int index) {
+    setState(() {
+      _focusedIndex = index;
+    });
+  }
+
+  Widget _buildItemDetail() {
+    if (data.length > _focusedIndex)
+      return Container(
+        height: 150,
+        child: Text("index $_focusedIndex: ${data[_focusedIndex]}"),
+      );
+
+    return Container(
+      height: 150,
+      child: Text("No Data"),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, int index) {
+    //horizontal
+    return Container(
+      width: 35,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Container(
+            height: data[index].toDouble() * 2,
+            width: 25,
+            color: Colors.lightBlueAccent,
+            child: Text("i:$index\n${data[index]}"),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,38 +83,34 @@ class _Question4WidgetState extends State<Question4Widget> {
       ),
       drawer: Drawer(
         child: ListView(
-          children: const <Widget>[
+          children: [
             ListTile(
               title: Text(
                 'Setting',
                 style: TextStyle(fontSize: 24),
               ),
             ),
+            Divider(),
             ListTile(
-              contentPadding: EdgeInsets.only(left: 70),
               title: Text('Backup & Restore'),
+              subtitle: Text("Sign in and synchronize your data"),
+              onTap: () {},
             ),
+            Divider(),
             ListTile(
-              contentPadding: EdgeInsets.only(top: 10, left: 15),
-              leading: Icon(Icons.person),
-              title: Text('Edit profile'),
-            ),
-            ListTile(
-              contentPadding: EdgeInsets.only(left: 15),
               leading: Icon(Icons.coffee),
               title: Text('Countdown Time'),
+              onTap: () => _AlertReTime(context),
             ),
             ListTile(
-              contentPadding: EdgeInsets.only(left: 15),
               leading: Icon(Icons.restore),
               title: Text('Restart progress'),
+              onTap: () => _AlertRestore(context),
             ),
           ],
         ),
       ),
-      body: Center(
-        child: Text("data"),
-      ),
+      body: screens[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (index) => setState(
@@ -65,9 +125,46 @@ class _Question4WidgetState extends State<Question4Widget> {
           BottomNavigationBarItem(
               icon: Icon(Icons.library_books_outlined),
               label: "History",
+              backgroundColor: Color.fromARGB(255, 196, 195, 195)),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: "Contact",
               backgroundColor: Color.fromARGB(255, 196, 195, 195))
         ],
       ),
     );
+  }
+
+  _AlertRestore(context) {
+    Alert(
+        context: context,
+        title: "Restart progress",
+        type: AlertType.warning,
+        buttons: [
+          DialogButton(
+              child: Text("OK"), onPressed: () => Navigator.pop(context)),
+          DialogButton(
+              child: Text("Cancel"), onPressed: () => Navigator.pop(context))
+        ]).show();
+  }
+
+  _AlertReTime(context) {
+    Alert(
+        context: context,
+        title: "Set Duration (5~180 secs)",
+        content: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Countdown'),
+              keyboardType: TextInputType.number,
+            )
+          ],
+        ),
+        buttons: [
+          DialogButton(
+              child: Text("OK"), onPressed: () => Navigator.pop(context)),
+          DialogButton(
+              child: Text("Cancel"), onPressed: () => Navigator.pop(context))
+        ]).show();
   }
 }
